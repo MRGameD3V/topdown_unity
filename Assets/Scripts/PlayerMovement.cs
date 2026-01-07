4939567cde578770f7de1bdc276ed27ebe1cfc6c
Marco Ramirez <m.r.gamed3v@gmail.com>
2026-01-06 19:11:00 -0600

    Added X/Y movement for player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 0.5f;
+    private Rigidbody2D rigidBody;
+    private Vector2 input;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+
+        input.Normalize();
+    }
+
+    private void FixedUpdate() {
+        rigidBody.velocity = input * speed;
     }
 }
